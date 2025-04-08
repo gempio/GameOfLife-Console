78ff6b7825de6b7ad01dad73e9ae766547252f82
Gempio <mmusialek1993@gmail.com>
2025-04-08 19:53:11 +0200

    Having fun with optimizations

--- game.go
+++ game.go
@@ -11,6 +11,8 @@ import (
 )
 
 func main() {
+	var cursorX, cursorY int8
+	var boardSize int8 = 8
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 
 	if err != nil {
@@ -19,7 +21,7 @@ func main() {
 
 	defer term.Restore(int(os.Stdin.Fd()), oldState)
 
-	display_game(0, oldState)
+	display_game(0, cursorX, cursorY, boardSize, oldState)
 	scanner := bufio.NewReader(os.Stdin)
 
 	for {
@@ -31,18 +33,22 @@ func main() {
 			panic(err)
 		}
 
-		display_game(char, oldState)
+		display_game(char, cursorX, cursorY, boardSize, oldState)
 	}
 }
 
-func display_game(char rune, oldState *term.State) {
+func display_game(char rune, cursorX int8, cursorY int8, boardSize int8, oldState *term.State) {
 	if char == 3 {
 		fmt.Println("\r\nFound some Ctrl+C\r")
 		term.Restore(int(os.Stdin.Fd()), oldState)
 		os.Exit(0)
 	} else {
 		fmt.Print("\033[H\033[2J\r")
-		game_logic.PrintBoard(5, 5)
+		fmt.Println("Welcome to game of life. For explanation how the game functions visit: https://en.wikipedia.org/wiki/Conway's_Game_of_Life\r")
+		fmt.Println("To start, move the cursor around the board using arrow keys and pressing \"space\" to change the state of a tile on a board.\r")
+		fmt.Println("To animate the board, press \"Enter\".\r")
+		fmt.Println()
+		game_logic.PrintBoard(boardSize, boardSize)
 		if char != 0 {
 			fmt.Printf("Selected char: %v \r\n", char)
 		}
--- game_logic/board.go
+++ game_logic/board.go
@@ -2,9 +2,12 @@ package game_logic
 
 import "fmt"
 
-func PrintBoard(x, y int) {
-	for i := 0; i < x; i++ {
-		for j := 0; j < y; j++ {
+func PrintBoard(x, y int8) {
+	var i int8 = 0
+	var j int8 = 0
+
+	for i = 0; i < x; i++ {
+		for j = 0; j < y; j++ {
 			fmt.Print("[ ] ")
 		}
 
