62760706999a3365d654306cbed8256ddb3edceb
Gempio <mmusialek1993@gmail.com>
2025-04-08 20:43:49 +0200

    Managing the cursor properly

--- game.go
+++ game.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	var cursorX, cursorY int8
+	var arrowUp, arrowDown, arrowRight, arrowLeft rune = 65, 66, 67, 68
 	var boardSize int8 = 8
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 
@@ -33,25 +34,60 @@ func main() {
 			panic(err)
 		}
 
+		switch char {
+		case arrowUp:
+			cursorY -= 1
+			break
+		case arrowDown:
+			cursorY += 1
+			break
+		case arrowRight:
+			cursorX += 1
+			break
+		case arrowLeft:
+			cursorX -= 1
+			break
+		}
+
+		if cursorY < 0 {
+			cursorY = boardSize - 1
+		}
+		if cursorY >= boardSize {
+			cursorY = 0
+		}
+		if cursorX < 0 {
+			cursorX = boardSize - 1
+		}
+		if cursorX >= boardSize {
+			cursorX = 0
+		}
+
 		display_game(char, cursorX, cursorY, boardSize, oldState)
 	}
 }
 
 func display_game(char rune, cursorX int8, cursorY int8, boardSize int8, oldState *term.State) {
 	if char == 3 {
-		fmt.Println("\r\nFound some Ctrl+C\r")
+		fmt.Print("\033[H\033[2J")
 		term.Restore(int(os.Stdin.Fd()), oldState)
 		os.Exit(0)
 	} else {
-		fmt.Print("\033[H\033[2J\r")
+		fmt.Print("\033[H\033[2J")
 		fmt.Println("Welcome to game of life. For explanation how the game functions visit: https://en.wikipedia.org/wiki/Conway's_Game_of_Life\r")
 		fmt.Println("To start, move the cursor around the board using arrow keys and pressing \"space\" to change the state of a tile on a board.\r")
 		fmt.Println("To animate the board, press \"Enter\".\r")
 		fmt.Println()
 		game_logic.PrintBoard(boardSize, boardSize)
-		if char != 0 {
-			fmt.Printf("Selected char: %v \r\n", char)
-		}
-		fmt.Print("Getting new char")
+
+		command := "\x1b["
+		command += string(5+cursorY) + ";"
+		command += string(2 + cursorX*4)
+		command += "H"
+
+		fmt.Fprint(os.Stdout, "\x1b[")
+		fmt.Fprint(os.Stdout, 5+cursorY)
+		fmt.Fprint(os.Stdout, ";")
+		fmt.Fprint(os.Stdout, 2+cursorX*4)
+		fmt.Fprint(os.Stdout, "H")
 	}
 }
